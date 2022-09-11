462a162ee0bad6b44a3716ae1926773d66f6a897
Edu-Hiroshi <97803912+Edu-Hiroshi@users.noreply.github.com>
2022-09-11 14:32:08 -0300

    Updated tb

--- Exp3/Atividade2/rx_serial_tb.vhd
+++ Exp3/Atividade2/rx_serial_tb.vhd
@@ -1,135 +1,156 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
+--------------------------------------------------------------------
+-- Arquivo   : rx_serial_tb.vhd
+-- Projeto   : Experiencia 3 - Recepcao Serial Assincrona
+--------------------------------------------------------------------
+-- Descricao : testbench para circuito de recepcao serial 
+--             contem recursos adicionais que devem ser aprendidos
+--             1) procedure em VHDL (UART_WRITE_BYTE)
+--             2) array de casos de teste
+-- 
+--------------------------------------------------------------------
+-- Revisoes  :
+--     Data        Versao  Autor             Descricao
+--     09/09/2021  1.0     Edson Midorikawa  versao inicial
+--     03/09/2022  1.1     Edson Midorikawa  revisao
+--     10/09/2022  1.2     Eduardo Hiroshi   adicao de casos de teste
+--------------------------------------------------------------------
+--
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-entity fluxo_dados is
-    port (
-        clock       : in std_logic;
-        reset       : in std_logic;
-        conta       : in std_logic;
-        zera        : in std_logic;
-        carrega     : in std_logic;
-        desloca     : in std_logic;
-        dado_serial : in std_logic;
-        limpa       : in std_logic;
-        registra    : in std_logic;
-        tick              : out std_logic;
-        fim               : out std_logic;
-        paridade_recebida : out std_logic;
-        paridade_ok       : out std_logic;
-        dados             : out std_logic_vector(7 downto 0)
-    );
+entity rx_serial_tb is
 end entity;
 
-architecture rtl of fluxo_dados is
-
-    component contador_m is
-        generic (
-            constant M : integer := 50;  
-            constant N : integer := 6 
+architecture tb of rx_serial_tb is
+  
+    -- Declaracao de sinais para conectar o componente a ser testado (DUT)
+    signal clock_in              : std_logic  := '0';
+    signal reset_in              : std_logic  := '0';
+    -- saidas
+    signal pronto_out            : std_logic  := '0';
+    signal paridade_recebida_out : std_logic  := '0';  -- new
+    signal temdado_out           : std_logic  := '0';
+    signal paridade_ok_out       : std_logic  := '0';  -- new
+  
+    -- para procedimento UART_WRITE_BYTE
+    signal entrada_serial_in : std_logic := '1';
+    signal serialData        : std_logic_vector(7 downto 0) := "00000000";
+  
+    -- Configuracoes do clock
+    constant clockPeriod : time := 20 ns;            -- 50MHz
+    -- constant bitPeriod   : time := 5208*clockPeriod; -- 5208 clocks por bit (9.600 bauds)
+    constant bitPeriod   : time := 434*clockPeriod;  -- 434 clocks por bit (115.200 bauds)
+    
+    ---- UART_WRITE_BYTE()
+    -- Procedimento para geracao da sequencia de comunicacao serial 8N2
+    -- adaptacao de codigo acessado de:
+    -- https://www.nandland.com/goboard/uart-go-board-project-part1.html
+    procedure UART_WRITE_BYTE (
+        Data_In : in  std_logic_vector(7 downto 0);
+        signal Serial_Out : out std_logic ) is
+    begin
+  
+        -- envia Start Bit
+        Serial_Out <= '0';
+        wait for bitPeriod;
+  
+        -- envia 8 bits seriais (dados + paridade)
+        for ii in 0 to 7 loop
+            Serial_Out <= Data_In(ii);
+            wait for bitPeriod;
+        end loop;  -- loop ii
+  
+        -- envia 2 Stop Bits
+        Serial_Out <= '1';
+        wait for 2*bitPeriod;
+  
+    end UART_WRITE_BYTE;
+    -- fim procedure
+  
+    ---- Array de casos de teste
+    type caso_teste_type is record
+        id   : natural;
+        data : std_logic_vector(7 downto 0);     
+    end record;
+  
+    type casos_teste_array is array (natural range <>) of caso_teste_type;
+    constant casos_teste : casos_teste_array :=
+        (
+            (1, "00110101"), -- 35H (paridade=0 + dado=35H) teste ok para 7E2
+            (2, "10110101"), -- B5H (paridade=1 + dado=35H) teste com erro para 7E2
+            (3, "10110111"), -- 37H (paridade=1 + dado=6EH) teste ok para 7E2
+            (4, "00110111")  -- B7H (paridade=0 + dado=6EH) teste com erro para 7E2
         );
-        port (
-            clock : in  std_logic;
-            zera  : in  std_logic;
-            conta : in  std_logic;
-            Q     : out std_logic_vector (N-1 downto 0);
-            fim   : out std_logic;
-            meio  : out std_logic
-        );
-    end component contador_m;
-
-    component deslocador_n is
-        generic (
-            constant N : integer := 4
-        );
-        port (
-            clock          : in  std_logic;
-            reset          : in  std_logic;
-            carrega        : in  std_logic; 
-            desloca        : in  std_logic; 
-            entrada_serial : in  std_logic; 
-            dados          : in  std_logic_vector (N-1 downto 0);
-            saida          : out std_logic_vector (N-1 downto 0)
-        );
-    end component deslocador_n;
-
-    component registrador_n is
-        generic (
-           constant N: integer := 8 
-        );
-        port (
-           clock  : in  std_logic;
-           clear  : in  std_logic;
-           enable : in  std_logic;
-           D      : in  std_logic_vector (N-1 downto 0);
-           Q      : out std_logic_vector (N-1 downto 0) 
-        );
-    end component registrador_n;
-
-    signal s_dados, s_saida: std_logic_vector(7 downto 0);
+    signal caso : natural;
+  
+    ---- controle do clock e simulacao
+    signal keep_simulating: std_logic := '0'; -- delimita o tempo de geracao do clock
+  
+  
 begin
+ 
+    ---- Gerador de Clock
+    clock_in <= (not clock_in) and keep_simulating after clockPeriod/2;
+    
+    -- Instanciacao direta DUT (Device Under Test)
+    DUT: entity work.rx_serial_7E2
+         port map (  
+             clock             => clock_in, 
+             reset             => reset_in,
+             dado_serial       => entrada_serial_in,
+             paridade_recebida => paridade_recebida_out,
+             pronto_rx         => pronto_out,
+             tem_dado          => temdado_out,
+             paridade_ok       => paridade_ok_out,
+             dado_recebido0    => open,
+             dado_recebido1    => open,
+             db_estado         => open
+         );
+    
+    ---- Geracao dos sinais de entrada (estimulo)
+    stimulus: process is
+    begin
+    
+        ---- inicio da simulacao
+        assert false report "inicio da simulacao" severity note;
+        keep_simulating <= '1';
+        -- reset com 5 periodos de clock
+        reset_in <= '0';
+        -- wait for bitPeriod;
+        reset_in <= '1', '0' after 5*clockPeriod; 
+        wait for bitPeriod;
+      
+        ---- loop pelos casos de teste
+        for i in casos_teste'range loop
+            caso <= casos_teste(i).id;
+            assert false report "Caso de teste " & integer'image(casos_teste(i).id) severity note;
+            serialData <= casos_teste(i).data; -- caso de teste "i"
+            -- aguarda 2 periodos de bit antes de enviar bits
+            wait for 2*bitPeriod;
+      
+            -- 1) envia bits seriais para circuito de recepcao
+            UART_WRITE_BYTE ( Data_In=>serialData, Serial_Out=>entrada_serial_in );
+            entrada_serial_in <= '1'; -- repouso
+            wait for bitPeriod;
+      
+            -- 2) intervalo entre casos de teste
+            wait for 2*bitPeriod;
+        end loop;
+      
+        ---- final dos casos de teste da simulacao
+        -- reset
+        reset_in <= '0';
+        wait for bitPeriod;
+        reset_in <= '1', '0' after 5*clockPeriod; 
+        wait for bitPeriod;
+      
+        ---- final da simulacao
+        assert false report "fim da simulacao" severity note;
+        keep_simulating <= '0';
+        
+        wait; -- fim da simulacao: aguarda indefinidamente
+    
+    end process stimulus;
 
-    -- gerador de tick
-    -- fator de divisao para 9600 bauds (5208=50M/9600)
-    -- fator de divisao para 115.200 bauds (434=50M/115200)
-    CONTADOR_TICK: contador_m 
-        generic map (
-            M => 434, -- 115.200 bauds
-            N => 13
-        ) 
-        port map (
-            clock => clock, 
-            zera  => zera, 
-            conta => '1', 
-            Q     => open, 
-            fim   => open,
-            meio  => tick
-        );
-
-    CONTADOR_DADOS: contador_m 
-        generic map (
-            M => 13, 
-            N => 4
-        ) 
-        port map (
-            clock => clock, 
-            zera  => zera, 
-            conta => conta, 
-            Q     => open, 
-            fim   => fim,
-            meio  => open
-        );
-
-    DESLOCADOR: deslocador_n 
-        generic map (
-            N => 8
-        ) 
-        port map (
-            clock          => clock, 
-            reset          => reset, 
-            carrega        => carrega, 
-            desloca        => desloca, 
-            entrada_serial => dado_serial, 
-            dados          => "00000000", 
-            saida          => s_saida
-        );
-
-    REGISTRADOR: registrador_n 
-        generic map (
-            N => 8
-        ) 
-        port map (
-            clock  => clock, 
-            clear  => limpa, 
-            enable => registra, 
-            D      => s_saida, 
-            Q      => s_dados
-        );
-
-    paridade_recebida <= s_dados(7);
-
-    paridade_ok <= not(s_dados(0) xor s_dados(1) xor s_dados(2) xor s_dados(3) xor
-                       s_dados(4) xor s_dados(5) xor s_dados(6) xor s_dados(7));
-
-    dados <= s_dados;
-
-end architecture rtl;
+end architecture tb;
