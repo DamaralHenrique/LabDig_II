6fbce342292d9b88ba21b10ce919fb100766c462
Eduardo Hiroshi <eduhito57@gmail.com>
2022-09-15 22:06:37 -0300

    Updated parts of contador_cm

--- Exp4/contador_cm.vhd
+++ Exp4/contador_cm.vhd
@@ -15,34 +15,34 @@ entity contador_cm is
         digito2 : out std_logic_vector(3 downto 0);
         pronto  : out std_logic
     );
-  end entity;
+end entity;
 
-  architecture rtl of interface_hcsr04_fd is
+architecture rtl of interface_hcsr04_fd is
 
     component contador_bcd_3digitos is 
-    port ( 
-        clock   : in  std_logic;
-        zera    : in  std_logic;
-        conta   : in  std_logic;
-        digito0 : out std_logic_vector(3 downto 0);
-        digito1 : out std_logic_vector(3 downto 0);
-        digito2 : out std_logic_vector(3 downto 0);
-        fim     : out std_logic
+        port ( 
+            clock   : in  std_logic;
+            zera    : in  std_logic;
+            conta   : in  std_logic;
+            digito0 : out std_logic_vector(3 downto 0);
+            digito1 : out std_logic_vector(3 downto 0);
+            digito2 : out std_logic_vector(3 downto 0);
+            fim     : out std_logic
     );
     end component contador_bcd_3digitos;
 
     component contador_m
-    generic (
-        constant M : integer;
-        constant N : integer
-    );
-    port (
-        clock : in  std_logic;
-        zera  : in  std_logic;
-        conta : in  std_logic;
-        Q     : out std_logic_vector (N-1 downto 0);
-        fim   : out std_logic
-    );
+        generic (
+            constant M : integer;
+            constant N : integer
+        );
+        port (
+            clock : in  std_logic;
+            zera  : in  std_logic;
+            conta : in  std_logic;
+            Q     : out std_logic_vector (N-1 downto 0);
+            fim   : out std_logic
+        );
     end component;
 
     component analisa_m is
@@ -59,38 +59,32 @@ entity contador_cm is
         );
     end component analisa_m;
 
-
-
-
-    -- signal s_dados: std_logic_vector(9 downto 0);
-    signal s_digito : std_logic_vector(11 downto 0);
-
-
+    signal s_valor : std_logic_vector(11 downto 0);
 
 begin
 
     CONTADOR_BCD: contador_bcd_3digitos
-    port map ( 
-        clock   => clock,
-        zera    => zera,
-        conta   => pulso, -- Valor do echo
-        digito0 => s_digito0,
-        digito1 => s_digito1,
-        digito2 => s_digito2,
-        fim     => open
-    );
+        port map ( 
+            clock   => clock,
+            zera    => zera,
+            conta   => pulso, -- Valor do echo
+            digito0 => s_digito0,
+            digito1 => s_digito1,
+            digito2 => s_digito2,
+            fim     => fim
+        );
 
     CONTADOR_M: contador_m 
         generic map (
-            M => 13, 
-            N => 4
+            M => 13, -- dummy
+            N => 4   -- dummy
         ) 
         port map (
             clock => clock, 
             zera  => zera, 
             conta => conta, 
-            Q     => s_digito, 
-            fim   => fim
+            Q     => s_valor, 
+            fim   => tick
         );
     
     ANALISA_MODULO_DE_ENTRADA: analisa_m
@@ -99,12 +93,11 @@ begin
             N => 6 
         );
         port map (
-            valor            => s_digito,
+            valor            => s_valor,
             zero             => open,
             meio             => open,
             fim              => open,
             metade_superior  => arredonda
         );
-
 
 end architecture rtl;
